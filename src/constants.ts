2a9b2dd3e0e1db20512d6ccac09e2a1ab0f0ae5d
onlynexd <106020262+onlynexd@users.noreply.github.com>
2024-07-08 02:12:05 +0300

    Update constants.ts

--- src/constants.ts
+++ src/constants.ts
@@ -53,8 +53,8 @@ export const DEFAULT_POOL = POOLS[0]
 export const TOKEN_METADATA: (Partial<TokenMeta> & {mint: PublicKey})[] = [
   {
     mint: FAKE_TOKEN_MINT,
-    name: 'Fake',
-    symbol: 'FAKE',
+    name: 'TL',
+    symbol: 'TL',
     image: '/fakemoney.png',
     baseWager: 1e9,
     decimals: 9,
